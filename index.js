4aeb34ca406649043edf9ae9577eb5edb8b9ef07
DorothyAriana <arianadottie@gmail.com>
2023-03-15 13:46:18 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+//function expression to divide a number
+const divide = function (num1, num2) {
+    num1 = 2000;
+    num2 = 100;
+    return num1 / num2;
+}
+
+//arrow function to square a number
+const square = num3 => num3 ** 2
+
+//arrow function to add a number
+const add = (num4, num5) => num4 + num5 
